2ed1ce59af474093fd11447a191cdd4a8e84fe75
James John McGuire <jamesjohnmcguire@gmail.com>
2025-01-03 00:31:33 +0900

    Add Initial Support for Reading Commands from JSON

--- SourceCode/CommandLineCommands/CommandsSet.cs
+++ SourceCode/CommandLineCommands/CommandsSet.cs
@@ -5,9 +5,11 @@
 /////////////////////////////////////////////////////////////////////////////
 
 using Common.Logging;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 
 namespace DigitalZenWorks.CommandLine.Commands
@@ -15,19 +17,45 @@ namespace DigitalZenWorks.CommandLine.Commands
 	/// <summary>
 	/// Represents a set of commands.
 	/// </summary>
-	/// <remarks>
-	/// Initializes a new instance of the
-	/// <see cref="CommandsSet"/> class.
-	/// </remarks>
-	/// <param name="commands">A list of valid commands.</param>
-	public class CommandsSet(IList<Command> commands)
+	public class CommandsSet
 	{
 		private static readonly ILog Log = LogManager.GetLogger(
 			System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
-		private readonly IList<Command> commands = commands;
+		private IList<Command> commands;
 
 		private bool useLog;
+
+		/// <summary>
+		/// Initializes a new instance of the
+		/// <see cref="CommandsSet"/> class.
+		/// </summary>
+		public CommandsSet()
+		{
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the
+		/// <see cref="CommandsSet"/> class.
+		/// </summary>
+		/// <param name="commands">A list of valid commands.</param>
+		public CommandsSet(IList<Command> commands)
+		{
+			this.commands = commands;
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the
+		/// <see cref="CommandsSet"/> class.
+		/// </summary>
+		/// <param name="commandsJson">A list of valid commands in
+		/// JSON format.</param>
+		public CommandsSet(string commandsJson)
+		{
+			IList<Command> commands =
+				JsonConvert.DeserializeObject<IList<Command>>(commandsJson);
+			this.commands = commands;
+		}
 
 		/// <summary>
 		/// Gets the list of commands.
@@ -51,6 +79,30 @@ namespace DigitalZenWorks.CommandLine.Commands
 		{
 			get { return useLog; }
 			set { useLog = value; }
+		}
+
+		/// <summary>
+		/// Read JSON commands from a file.
+		/// </summary>
+		/// <param name="filePath">The file to read from.</param>
+		/// <exception cref="FileNotFoundException">Thrown if file is
+		/// not found.</exception>
+		public void JsonFromFile(string filePath)
+		{
+			bool exists = File.Exists(filePath);
+
+			if (exists == true)
+			{
+				string jsonText = File.ReadAllText(filePath);
+
+				IList<Command> commands =
+					JsonConvert.DeserializeObject<IList<Command>>(jsonText);
+				this.commands = commands;
+			}
+			else
+			{
+				throw new FileNotFoundException("File Not Found.");
+			}
 		}
 
 		/// <summary>
