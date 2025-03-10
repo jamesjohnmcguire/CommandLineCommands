4e8af66af8981437770d249197ebcdba2e1db119
James John McGuire <jamesjohnmcguire@gmail.com>
2025-03-10 21:02:21 +0900

    Remove Duplicated Code

--- SourceCode/CommandLineCommands/CommandLineArguments.cs
+++ SourceCode/CommandLineCommands/CommandLineArguments.cs
@@ -28,7 +28,6 @@ namespace DigitalZenWorks.CommandLine.Commands
 		private readonly IList<Command> commandsList;
 		private readonly InferCommand inferCommand;
 
-		private string errorMessage;
 		private bool useLog;
 
 		/// <summary>
@@ -41,10 +40,6 @@ namespace DigitalZenWorks.CommandLine.Commands
 		public CommandLineArguments(
 			IList<Command> commands, string[] arguments)
 		{
-			this.commandsList = commands;
-			this.commands = new CommandsSet(commands);
-			this.arguments = arguments;
-
 			commandLineInstance = new (this.commands, arguments);
 		}
 
@@ -57,9 +52,6 @@ namespace DigitalZenWorks.CommandLine.Commands
 		/// arguments.</param>
 		public CommandLineArguments(CommandsSet commands, string[] arguments)
 		{
-			this.commands = commands;
-			this.arguments = arguments;
-
 			commandLineInstance = new (commands, arguments);
 		}
 
@@ -94,9 +86,6 @@ namespace DigitalZenWorks.CommandLine.Commands
 			InferCommand inferCommand)
 		{
 			this.inferCommand = inferCommand;
-
-			this.commands = commands;
-			this.arguments = arguments;
 		}
 
 		/// <summary>
@@ -109,7 +98,10 @@ namespace DigitalZenWorks.CommandLine.Commands
 		/// Gets the error message, if any.
 		/// </summary>
 		/// <value>The error message, if any.</value>
-		public string ErrorMessage { get { return errorMessage; } }
+		public string ErrorMessage
+		{
+			get { return commandLineInstance.ErrorMessage; }
+		}
 
 		/// <summary>
 		/// Gets or sets the usage statement.
@@ -145,107 +137,7 @@ namespace DigitalZenWorks.CommandLine.Commands
 		/// <param name="title">An optional title to display.</param>
 		public void ShowHelp(string title = null)
 		{
-			if (!string.IsNullOrWhiteSpace(title))
-			{
-				Output(title);
-				Output(string.Empty);
-			}
-
-			Output("Usage:");
-			Output(UsageStatement);
-			Output(string.Empty);
-
-			int commandMaximumLength = 0;
-			int descriptionMaximumLength = 0;
-
-			foreach (Command command in commandsList)
-			{
-				commandMaximumLength = GetMaximumLength(
-					commandMaximumLength, command.Name);
-				descriptionMaximumLength = GetMaximumLength(
-					descriptionMaximumLength, command.Description);
-			}
-
-			Command help = commandsList.SingleOrDefault(x => x.Name == "help");
-
-			commandsList.Remove(help);
-
-			IOrderedEnumerable<Command> sortedCommands =
-				commandsList.OrderBy(x => x.Name);
-
-			foreach (Command command in sortedCommands)
-			{
-				string options = string.Empty;
-				bool first = true;
-
-				if (command.Options != null)
-				{
-					foreach (CommandOption option in command.Options)
-					{
-						if (first == true)
-						{
-							first = false;
-						}
-						else
-						{
-							options += Environment.NewLine;
-
-							int paddingAmount = commandMaximumLength +
-									descriptionMaximumLength + 2;
-							string padding = string.Empty;
-							padding = padding.PadRight(paddingAmount, ' ');
-							options += padding;
-						}
-
-						string optionMessage = string.Format(
-							CultureInfo.InvariantCulture,
-							"-{0}, --{1}",
-							option.ShortName,
-							option.LongName);
-						options += optionMessage;
-					}
-				}
-
-				string message = string.Format(
-					CultureInfo.InvariantCulture,
-					"{0} {1} {2}",
-					command.Name.PadRight(commandMaximumLength, ' '),
-					command.Description.PadRight(
-						descriptionMaximumLength, ' '),
-					options);
-				Output(message);
-			}
-
-			if (help != null)
-			{
-				string helpMessage = string.Format(
-					CultureInfo.InvariantCulture,
-					"{0} {1}",
-					help.Name.PadRight(commandMaximumLength, ' '),
-					help.Description.PadRight(
-						descriptionMaximumLength, ' '));
-				Output(helpMessage);
-			}
-		}
-
-		private static int GetMaximumLength(
-			int previousMaximumLength, string text)
-		{
-			int maximumLength = Math.Max(previousMaximumLength, text.Length);
-
-			return maximumLength;
-		}
-
-		private void Output(string message)
-		{
-			if (useLog == true)
-			{
-				Log.Info(message);
-			}
-			else
-			{
-				Console.WriteLine(message);
-			}
+			commandLineInstance.ShowHelp(title);
 		}
 	}
 }
