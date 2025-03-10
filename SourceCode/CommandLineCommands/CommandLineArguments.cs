870df673578ccd9d8dc262e60718ab8d6d30fc99
James John McGuire <jamesjohnmcguire@gmail.com>
2025-03-10 15:12:39 +0900

    Add Back Functionality to Fix APIs

--- SourceCode/CommandLineCommands/CommandLineArguments.cs
+++ SourceCode/CommandLineCommands/CommandLineArguments.cs
@@ -28,8 +28,12 @@ namespace DigitalZenWorks.CommandLine.Commands
 	/// </summary>
 	public class CommandLineArguments
 	{
+		private static readonly ILog Log = LogManager.GetLogger(
+			System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
 		private readonly string[] arguments;
 		private readonly CommandsSet commands;
+		private readonly IList<Command> commandsList;
 		private readonly InferCommand inferCommand;
 		private readonly bool validArguments;
 
@@ -43,12 +47,49 @@ namespace DigitalZenWorks.CommandLine.Commands
 		/// Initializes a new instance of the
 		/// <see cref="CommandLineArguments"/> class.
 		/// </summary>
+		/// <param name="commands">A list of valid commands.</param>
+		/// <param name="arguments">The array of command line
+		/// arguments.</param>
+		public CommandLineArguments(
+			IList<Command> commands, string[] arguments)
+		{
+			this.commandsList = commands;
+			this.arguments = arguments;
+
+			validArguments = ValidateArguments();
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the
+		/// <see cref="CommandLineArguments"/> class.
+		/// </summary>
 		/// <param name="commands">The set of valid commands.</param>
 		/// <param name="arguments">The array of command line
 		/// arguments.</param>
 		public CommandLineArguments(CommandsSet commands, string[] arguments)
 		{
 			this.commands = commands;
+			this.arguments = arguments;
+
+			validArguments = ValidateArguments();
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the
+		/// <see cref="CommandLineArguments"/> class.
+		/// </summary>
+		/// <param name="commands">A list of valid commands.</param>
+		/// <param name="arguments">The array of command line
+		/// arguments.</param>
+		/// <param name="inferCommand">The infer command delegate.</param>
+		public CommandLineArguments(
+			IList<Command> commands,
+			string[] arguments,
+			InferCommand inferCommand)
+		{
+			this.inferCommand = inferCommand;
+
+			this.commandsList = commands;
 			this.arguments = arguments;
 
 			validArguments = ValidateArguments();
@@ -111,6 +152,103 @@ namespace DigitalZenWorks.CommandLine.Commands
 		/// <value>A value indicating whether the arguments are valid
 		/// or not.</value>
 		public bool ValidArguments { get { return validArguments; } }
+
+		/// <summary>
+		/// Show help message.
+		/// </summary>
+		/// <param name="title">An optional title to display.</param>
+		public void ShowHelp(string title = null)
+		{
+			if (!string.IsNullOrWhiteSpace(title))
+			{
+				Output(title);
+				Output(string.Empty);
+			}
+
+			Output("Usage:");
+			Output(UsageStatement);
+			Output(string.Empty);
+
+			int commandMaximumLength = 0;
+			int descriptionMaximumLength = 0;
+
+			foreach (Command command in commandsList)
+			{
+				commandMaximumLength = GetMaximumLength(
+					commandMaximumLength, command.Name);
+				descriptionMaximumLength = GetMaximumLength(
+					descriptionMaximumLength, command.Description);
+			}
+
+			Command help = commandsList.SingleOrDefault(x => x.Name == "help");
+
+			commandsList.Remove(help);
+
+			IOrderedEnumerable<Command> sortedCommands =
+				commandsList.OrderBy(x => x.Name);
+
+			foreach (Command command in sortedCommands)
+			{
+				string options = string.Empty;
+				bool first = true;
+
+				if (command.Options != null)
+				{
+					foreach (CommandOption option in command.Options)
+					{
+						if (first == true)
+						{
+							first = false;
+						}
+						else
+						{
+							options += Environment.NewLine;
+
+							int paddingAmount = commandMaximumLength +
+									descriptionMaximumLength + 2;
+							string padding = string.Empty;
+							padding = padding.PadRight(paddingAmount, ' ');
+							options += padding;
+						}
+
+						string optionMessage = string.Format(
+							CultureInfo.InvariantCulture,
+							"-{0}, --{1}",
+							option.ShortName,
+							option.LongName);
+						options += optionMessage;
+					}
+				}
+
+				string message = string.Format(
+					CultureInfo.InvariantCulture,
+					"{0} {1} {2}",
+					command.Name.PadRight(commandMaximumLength, ' '),
+					command.Description.PadRight(
+						descriptionMaximumLength, ' '),
+					options);
+				Output(message);
+			}
+
+			if (help != null)
+			{
+				string helpMessage = string.Format(
+					CultureInfo.InvariantCulture,
+					"{0} {1}",
+					help.Name.PadRight(commandMaximumLength, ' '),
+					help.Description.PadRight(
+						descriptionMaximumLength, ' '));
+				Output(helpMessage);
+			}
+		}
+
+		private static int GetMaximumLength(
+			int previousMaximumLength, string text)
+		{
+			int maximumLength = Math.Max(previousMaximumLength, text.Length);
+
+			return maximumLength;
+		}
 
 		private static bool IsHelpCommend(string command)
 		{
@@ -349,6 +487,18 @@ namespace DigitalZenWorks.CommandLine.Commands
 			}
 
 			return areValid;
+		}
+
+		private void Output(string message)
+		{
+			if (useLog == true)
+			{
+				Log.Info(message);
+			}
+			else
+			{
+				Console.WriteLine(message);
+			}
 		}
 	}
 }
