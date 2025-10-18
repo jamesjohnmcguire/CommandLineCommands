5cc7108a55698edea9ed922868ec16ac627fc1fe
James John McGuire <jamesjohnmcguire@gmail.com>
2025-10-18 16:52:47 +0900

    Add HelpManager Class with Tests

---
+++ CommandLineCommands.Tests/HelpManagerTests.cs
@@ -0,0 +1,263 @@
+﻿/////////////////////////////////////////////////////////////////////////////
+// <copyright file="HelpManagerTests.cs" company="James John McGuire">
+// Copyright © 2022 - 2025 James John McGuire. All Rights Reserved.
+// </copyright>
+/////////////////////////////////////////////////////////////////////////////
+
+namespace DigitalZenWorks.CommandLine.Commands.Tests
+{
+	using DigitalZenWorks.Common.Utilities;
+	using NUnit.Framework;
+	using System;
+	using System.Collections.Generic;
+	using System.Globalization;
+	using System.IO;
+
+	/// <summary>
+	/// The help manager tests class.
+	/// </summary>
+	internal sealed class HelpManagerTests
+	{
+		private CommandsSet commandsSetFull;
+
+		private CommandsSet commandsSetMinimal;
+
+		private CommandsSet commandsSetNoOptions;
+
+		private CommandsSet commandsSetNoParameters;
+
+		private CommandsSet commandsSetVerySimple;
+
+		private string topHeaderText;
+
+		/// <summary>
+		/// One time set up method.
+		/// </summary>
+		[OneTimeSetUp]
+		public void OneTimeSetUp()
+		{
+			string title = "CommandLine Commands Tests";
+
+			topHeaderText = string.Format(
+				CultureInfo.InvariantCulture,
+				"{0}{1}{1}Usage:{1}",
+				title,
+				Environment.NewLine);
+
+			List<Command> commands = [];
+
+			Command command = new("help");
+			command.Description = "Show this information";
+			commands.Add(command);
+
+			List<Command> commandsMinimal = new List<Command>(commands);
+
+			commandsSetMinimal = GetCommandSetMinimal();
+
+			command = new ("check");
+			command.Description = "Check file for some text";
+
+			List<string> parameters = ["file path"];
+			command.Parameters = parameters;
+
+			commands.Add(command);
+
+			commandsSetVerySimple = GetCommandSetVerySimple();
+
+			Command convert = new ("convert");
+			convert.Description = "Convert file for some reason";
+
+			parameters = ["input file path", "output file path"];
+			convert.Parameters = parameters;
+
+			commands.Add(convert);
+
+			commandsSetNoOptions = GetCommandSetNoOptions();
+
+			commands = new List<Command>(commandsMinimal);
+			CommandOption option1 = new("s", "something");
+			CommandOption option2 = new("o", "other");
+			List<CommandOption> options = [option1, option2];
+
+			command = new(
+				"command-no-parameters",
+				options,
+				0,
+				"A command with only options");
+			commands.Add(command);
+
+			commandsSetNoParameters = GetCommandSetNoParameters();
+
+			commands.Add(convert);
+			commandsSetFull = new CommandsSet(commands);
+		}
+
+		/// <summary>
+		/// One time tear down method.
+		/// </summary>
+		[OneTimeTearDown]
+		public void OneTimeTearDown()
+		{
+		}
+
+		/// <summary>
+		/// Set up method.
+		/// </summary>
+		[SetUp]
+		public void Setup()
+		{
+		}
+
+		/// <summary>
+		/// Get help header text minimal test.
+		/// </summary>
+		[Test]
+		public void GetHelpHeaderTextMinimal()
+		{
+			string expected = string.Format(
+				CultureInfo.InvariantCulture,
+				"{0}Command Description{1}",
+				topHeaderText,
+				Environment.NewLine);
+
+			HelpManager helpManager = new (commandsSetMinimal);
+			string actual = helpManager.GetHelpHeaderText(
+				"CommandLine Commands Tests");
+
+			Assert.That(actual, Is.EqualTo(expected));
+		}
+
+		/// <summary>
+		/// Get help header text no option test.
+		/// </summary>
+		[Test]
+		public void GetHelpHeaderTextNoOptions()
+		{
+			string expected = string.Format(
+				CultureInfo.InvariantCulture,
+				"{0}Command Description                  Parameters{1}",
+				topHeaderText,
+				Environment.NewLine);
+
+			HelpManager helpManager = new (commandsSetNoOptions);
+			string actual = helpManager.GetHelpHeaderText(
+				"CommandLine Commands Tests");
+
+			Assert.That(actual, Is.EqualTo(expected));
+		}
+
+		/// <summary>
+		/// Get help header text no option test.
+		/// </summary>
+		[Test]
+		public void GetHelpHeaderTextNoParameters()
+		{
+			string expected = string.Format(
+				CultureInfo.InvariantCulture,
+				"{0}Command         Description                 Options{1}",
+				topHeaderText,
+				Environment.NewLine);
+
+			HelpManager helpManager = new (commandsSetNoParameters);
+			string actual = helpManager.GetHelpHeaderText(
+				"CommandLine Commands Tests");
+
+			Assert.That(actual, Is.EqualTo(expected));
+		}
+
+		/// <summary>
+		/// Get help header text very simple test.
+		/// </summary>
+		[Test]
+		public void GetHelpHeaderTextVerySimple()
+		{
+			string expected = string.Format(
+				CultureInfo.InvariantCulture,
+				"{0}Command Description              Parameters{1}",
+				topHeaderText,
+				Environment.NewLine);
+
+			HelpManager helpManager = new (commandsSetVerySimple);
+			string actual = helpManager.GetHelpHeaderText(
+				"CommandLine Commands Tests");
+
+			Assert.That(actual, Is.EqualTo(expected));
+		}
+
+		private CommandsSet GetCommandSetMinimal()
+		{
+			List<Command> commands = [];
+
+			Command command = new ("help");
+			command.Description = "Show this information";
+			commands.Add(command);
+
+			CommandsSet commandsSet = new CommandsSet(commands);
+
+			return commandsSet;
+		}
+
+		private CommandsSet GetCommandSetNoOptions()
+		{
+			CommandsSet commandsSetMinimal = GetCommandSetVerySimple();
+
+			List<Command> commands =
+				new List<Command>(commandsSetMinimal.Commands);
+
+			Command convert = new ("convert");
+			convert.Description = "Convert file for some reason";
+
+			string[] parameters = ["input file path", "output file path"];
+			convert.Parameters = parameters;
+
+			commands.Add(convert);
+
+			CommandsSet commandsSet = new CommandsSet(commands);
+
+			return commandsSet;
+		}
+
+		private CommandsSet GetCommandSetNoParameters()
+		{
+			CommandsSet commandsSetMinimal = GetCommandSetMinimal();
+
+			List<Command> commands =
+				new List<Command>(commandsSetMinimal.Commands);
+
+			CommandOption option1 = new ("s", "something");
+			CommandOption option2 = new ("o", "other");
+			List<CommandOption> options = [option1, option2];
+
+			Command command = new (
+				"command-options",
+				options,
+				0,
+				"A command with only options");
+			commands.Add(command);
+
+			CommandsSet commandsSet = new CommandsSet(commands);
+
+			return commandsSet;
+		}
+
+		private CommandsSet GetCommandSetVerySimple()
+		{
+			CommandsSet commandsSetMinimal = GetCommandSetMinimal();
+
+			List<Command> commands =
+				new List<Command>(commandsSetMinimal.Commands);
+
+			Command command = new ("check");
+			command.Description = "Check file for some text";
+
+			List<string> parameters = ["file path"];
+			command.Parameters = parameters;
+
+			commands.Add(command);
+
+			CommandsSet commandsSet = new CommandsSet(commands);
+
+			return commandsSet;
+		}
+	}
+}
---
+++ CommandLineCommands/HelpManager.cs
@@ -0,0 +1,238 @@
+﻿/////////////////////////////////////////////////////////////////////////////
+// <copyright file="HelpManager.cs" company="James John McGuire">
+// Copyright © 2022 - 2025 James John McGuire. All Rights Reserved.
+// </copyright>
+/////////////////////////////////////////////////////////////////////////////
+
+#nullable enable
+
+namespace DigitalZenWorks.CommandLine.Commands
+{
+	using System;
+	using System.Globalization;
+	using System.Xml.Linq;
+
+	/// <summary>
+	/// The help manager class.
+	/// </summary>
+	public class HelpManager
+	{
+		private readonly CommandsSet commandSet;
+
+		// For 'Command' length.
+		private int commandColumnLength = 7;
+
+		private int descriptionColumnLength;
+
+		private int optionsColumnLength;
+
+		private int parametersColumnLength;
+
+		/// <summary>
+		/// Initializes a new instance of the
+		/// <see cref="HelpManager"/> class.
+		/// </summary>
+		/// <param name="commandSet">The command set.</param>
+		public HelpManager(CommandsSet commandSet)
+		{
+			this.commandSet = commandSet;
+
+			if (commandSet != null)
+			{
+				foreach (Command command in commandSet.Commands)
+				{
+					string description = command.Description;
+					string name = command.Name;
+
+					commandColumnLength =
+						Math.Max(name.Length, commandColumnLength);
+					descriptionColumnLength =
+						Math.Max(description.Length, descriptionColumnLength);
+
+					optionsColumnLength = GetOptionsMaximumLength(command);
+					parametersColumnLength =
+						GetParametersMaximumLength(command);
+				}
+			}
+		}
+
+		/// <summary>
+		/// Get the help header text.
+		/// </summary>
+		/// <param name="title">The title of the application.</param>
+		/// <returns>The help header text.</returns>
+		public string GetHelpHeaderText(string title)
+		{
+			string headerText = string.Format(
+				CultureInfo.InvariantCulture,
+				"{0}{1}{1}Usage:{1}",
+				title,
+				Environment.NewLine);
+
+			headerText += GetHeaderColumns();
+			headerText += Environment.NewLine;
+
+			return headerText;
+		}
+
+		private static int GetOptionsMaximumLength(
+			int currentColumnLength, Command command)
+		{
+			int optionsColumnLength = currentColumnLength;
+
+			if (command.Options != null)
+			{
+				foreach (CommandOption option in command.Options)
+				{
+					string optionName = option.LongName;
+
+					optionsColumnLength = Math.Max(
+						option.LongName.Length, optionsColumnLength);
+				}
+			}
+
+			return optionsColumnLength;
+		}
+
+		private static int GetParametersMaximumLength(
+			int currentColumnLength, Command command)
+		{
+			int parametersColumnLength = currentColumnLength;
+
+			if (command.Parameters != null)
+			{
+				foreach (string parameter in command.Parameters)
+				{
+					parametersColumnLength = Math.Max(
+						parameter.Length, parametersColumnLength);
+				}
+			}
+
+			return parametersColumnLength;
+		}
+
+		private static string PadColumn(string column, int columnLength)
+		{
+			int paddingLength =
+				Math.Max(column.Length, columnLength);
+
+			// For column separator.
+			paddingLength++;
+
+			column = column.PadRight(paddingLength);
+
+			return column;
+		}
+
+		private string GetHeaderColumns()
+		{
+			string columnName = "Command";
+			string headerColumns = columnName;
+			bool addPadding = false;
+
+			if (descriptionColumnLength > 0 || optionsColumnLength > 0 ||
+				parametersColumnLength > 0)
+			{
+				headerColumns = PadColumn(headerColumns, commandColumnLength);
+
+				if (descriptionColumnLength > 0)
+				{
+					columnName = "Description";
+
+					if (optionsColumnLength > 0 || parametersColumnLength > 0)
+					{
+						addPadding = true;
+
+						// Compensate for the header name too.
+						descriptionColumnLength =
+							Math.Max(columnName.Length, descriptionColumnLength);
+					}
+
+					string columnText = GetPaddedColumn(
+						columnName, descriptionColumnLength, addPadding);
+					headerColumns += columnText;
+
+					if (optionsColumnLength > 0)
+					{
+						columnName = "Options";
+						headerColumns += columnName;
+
+						if (parametersColumnLength > 0)
+						{
+							headerColumns += "Parameters";
+						}
+					}
+					else if (parametersColumnLength > 0)
+					{
+						headerColumns += "Parameters";
+					}
+				}
+			}
+
+			return headerColumns;
+		}
+
+		private string GetHeaderNameColumn()
+		{
+			string nameColumn = "Command";
+
+			return nameColumn;
+		}
+
+		private int GetOptionsMaximumLength(Command command)
+		{
+			if (command.Options != null)
+			{
+				foreach (CommandOption option in command.Options)
+				{
+					string optionName = option.LongName;
+
+					optionsColumnLength = Math.Max(
+						option.LongName.Length, optionsColumnLength);
+				}
+			}
+
+			return optionsColumnLength;
+		}
+
+		private string AddParametersHeaderColumn(string headerColumns)
+		{
+			string text = string.Empty;
+
+			if (parametersColumnLength > 0)
+			{
+				headerColumns = PadColumn(
+					headerColumns, optionsColumnLength);
+
+				headerColumns += "Parameters";
+			}
+
+			return headerColumns;
+		}
+
+		private string GetPaddedColumn(
+			string columnText, int columnLength, bool addPadding)
+		{
+			if (addPadding == true)
+			{
+				columnText = PadColumn(columnText, columnLength);
+			}
+
+			return columnText;
+		}
+
+		private int GetParametersMaximumLength(Command command)
+		{
+			if (command.Parameters != null)
+			{
+				foreach (string parameter in command.Parameters)
+				{
+					parametersColumnLength = Math.Max(
+						parameter.Length, parametersColumnLength);
+				}
+			}
+
+			return parametersColumnLength;
+		}
+	}
+}
